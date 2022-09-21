3987b2ec10706645377f8e5c9f84898dbd6097d4
acloop62hz <wanmei326@gmail.com>
2022-09-21 15:12:11 -0400

    ee2

--- Starter Code/jupiter/gravity.go
+++ Starter Code/jupiter/gravity.go
@@ -18,10 +18,28 @@ func SimulateGravity(initialUniverse Universe, numGen int, time float64) []Unive
 	timepoints := make([]Universe, numGen+1)
 	timepoints[0] = initialUniverse
 	for i := 1; i <= numGen; i++ {
-		timepoints[i] = UpdateUniverse(timepoints[i-1])
+		timepoints[i] = UpdateUniverse(timepoints[i-1], time)
 	}
+	return timepoints
 }
 
 func UpdateUniverse(currentUniverse Universe, time float64) Universe {
+	newUniverse := currentUniverse
+	for i, b := range currentUniverse {
+		b.acceleration = UpdateAcceleration()
+	}
+}
 
+func UpdateVelocity(b Body, time float64) OrderedPair {
+	var v OrderedPair
+	v.x = b.acceleration.x*time + b.velocity.x
+	v.y = b.acceleration.y*time + b.velocity.y
+	return v
+}
+
+func UpdatePosition(b Body, time float64) OrderedPair {
+	var p OrderedPair
+	p.x = 1.0/2.0*b.acceleration.x*time*time + b.velocity.x*time + b.position.x
+	p.y = 1.0/2.0*b.acceleration.y*time*time + b.velocity.y*time + b.position.y
+	return p
 }
