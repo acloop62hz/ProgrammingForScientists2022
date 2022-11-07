d7fe36bcc45675f5c88dd40e4e83c6ffaef35917
acloop62hz <wanmei326@gmail.com>
2022-11-07 04:34:10 -0500

    debug hw2

--- Completed Code/go/src/BarnesHut/functions.go
+++ Completed Code/go/src/BarnesHut/functions.go
@@ -1,9 +1,21 @@
 package main
 
-//BarnesHut is our highest level function.
-//Input: initial Universe object, a number of generations, and a time interval.
-//Output: collection of Universe objects corresponding to updating the system
-//over indicated number of generations every given time interval.
+import (
+	"fmt"
+	"math"
+)
+
+func (galaxy *Galaxy) Push(x, y float64) {
+	for i := range *galaxy {
+		(*galaxy)[i].acceleration.x = x
+		(*galaxy)[i].acceleration.x = y
+	}
+}
+
+// BarnesHut is our highest level function.
+// Input: initial Universe object, a number of generations, and a time interval.
+// Output: collection of Universe objects corresponding to updating the system
+// over indicated number of generations every given time interval.
 func BarnesHut(initialUniverse *Universe, numGens int, time, theta float64) []*Universe {
 	timePoints := make([]*Universe, numGens+1)
 	timePoints[0] = initialUniverse
@@ -11,21 +23,24 @@ func BarnesHut(initialUniverse *Universe, numGens int, time, theta float64) []*U
 	//now range over the number of generations and update the universe each time
 	for i := 1; i <= numGens; i++ {
 		timePoints[i] = timePoints[i-1].UpdateUniverse(time, theta)
+		fmt.Println(i)
 	}
 
 	return timePoints
 }
 
-//UpdateUniverse updates a given universe over a specified time interval (in seconds).
-//Input: A Universe object, a float time and Barnes-Hut parameter theta
-//Output: A Universe object corresponding to simulating gravity over time seconds
+// UpdateUniverse updates a given universe over a specified time interval (in seconds).
+// Input: A Universe object, a float time and Barnes-Hut parameter theta
+// Output: A Universe object corresponding to simulating gravity over time seconds
 func (currentUniverse *Universe) UpdateUniverse(time, theta float64) *Universe {
 	newUniverse := currentUniverse.CopyUniverse()
 
+	var quadTree QuadTree
+	quadTree = currentUniverse.BuildQuadtree()
 	//range over all stars in the universe and update their acceleration,
 	//velocity, and position
 	for i := range newUniverse.stars {
-		newUniverse.stars[i].UpdateAcceleration(currentUniverse,theta)
+		newUniverse.stars[i].UpdateAcceleration(currentUniverse, &quadTree, theta)
 		newUniverse.stars[i].UpdateVelocity(time)
 		newUniverse.stars[i].UpdatePosition(time)
 	}
@@ -33,27 +48,82 @@ func (currentUniverse *Universe) UpdateUniverse(time, theta float64) *Universe {
 	return &newUniverse
 }
 
-func (star *Star) UpdateAcceleration(currentUniverse *Universe, theta float64) {
-	var quadTree *QuadTree
-	*quadTree = currentUniverse.BuildQuadtree()
+func (star *Star) UpdateAcceleration(currentUniverse *Universe, quadTree *QuadTree, theta float64) {
 
-	force := ComputeNetForce(quadTree, star)
+	force := ComputeNetForce(quadTree.root, star, theta)
 
-	star.acceleration.x = force.x/star.mass
-	star.acceleration.y = force.y/star.mass
-	
+	star.acceleration.x = force.x / star.mass
+	star.acceleration.y = force.y / star.mass
+
 }
 
-func ComputeNetForce(quadTree *QuadTree, star *Star) OrderedPair{
-	for i := 0; i < 4; i++{
-		if quadTree.
+func (star *Star) UpdateVelocity(time float64) {
+	//new velocity is current velocity + acceleration * time
+	star.velocity.x = star.velocity.x + star.acceleration.x*time
+	star.velocity.y = star.velocity.y + star.acceleration.y*time
+}
+
+// UpdatePosition
+// Input: a Body b and a time step (float64).
+// Output: The OrderedPair corresponding to the updated position of the body after a single time step, using the body's current acceleration and velocity.
+func (star *Star) UpdatePosition(time float64) {
+	star.position.x = 0.5*star.acceleration.x*time*time + star.velocity.x*time + star.position.x
+	star.position.y = 0.5*star.acceleration.y*time*time + star.velocity.y*time + star.position.y
+}
+
+func ComputeNetForce(root *Node, star *Star, theta float64) OrderedPair {
+	var force OrderedPair
+	force.x = 0
+	force.y = 0
+	if root == nil {
+		return force
+	} else if root.children == nil {
+		if root.star == nil || *(root.star) == *(star) {
+			return force
+		} else {
+			force = ComputeSingleStarForce(root.star, star)
+		}
+	} else {
+		dist := Distance(root.star.position, star.position)
+		if root.sector.width/dist > theta {
+			for i := 0; i < 4; i++ {
+				force.x += ComputeNetForce(root.children[i], star, theta).x
+				force.y += ComputeNetForce(root.children[i], star, theta).y
+			}
+		} else {
+			force.x += ComputeSingleStarForce(root.star, star).x
+			force.y += ComputeSingleStarForce(root.star, star).y
+		}
 	}
+	return force
 }
 
-//CopyUniverse
-//Input: a Universe object
-//Output: a new Universe object, all of whose fields are copied over
-//into the new Universe's fields. (Deep copy.)
+func ComputeSingleStarForce(caster, receiver *Star) OrderedPair {
+	var force OrderedPair
+
+	dist := Distance(caster.position, receiver.position)
+	dx := caster.position.x - receiver.position.x
+	dy := caster.position.y - receiver.position.y
+
+	F := G * caster.mass * receiver.mass / (dist * dist)
+	force.x = F * dx / dist
+	force.y = F * dy / dist
+
+	return force
+
+}
+
+func Distance(a, b OrderedPair) float64 {
+	dx := a.x - b.x
+	dy := a.y - b.y
+	d := math.Sqrt(dx*dx + dy*dy)
+	return d
+}
+
+// CopyUniverse
+// Input: a Universe object
+// Output: a new Universe object, all of whose fields are copied over
+// into the new Universe's fields. (Deep copy.)
 func (currentUniverse *Universe) CopyUniverse() Universe {
 	var newUniverse Universe
 
@@ -61,7 +131,12 @@ func (currentUniverse *Universe) CopyUniverse() Universe {
 
 	//let's make the new universe's slice of Star objects
 	numStars := len(currentUniverse.stars)
+	stars := make([]Star, numStars)
 	newUniverse.stars = make([]*Star, numStars)
+
+	for i := range newUniverse.stars {
+		newUniverse.stars[i] = &stars[i]
+	}
 
 	//now, copy all of the stars' fields into our new stars
 	for i := range currentUniverse.stars {
@@ -85,20 +160,19 @@ func (currentUniverse *Universe) BuildQuadtree() QuadTree {
 	//initialize root node
 	var root Node
 	root.sector.width = currentUniverse.width
-	root.sector.x = currentUniverse.width / 2
-	root.sector.y = currentUniverse.width / 2
+	root.sector.x = currentUniverse.width / 2.0
+	root.sector.y = currentUniverse.width / 2.0
 
 	//initialize the quadtree
 	var quadTree QuadTree
 	quadTree.root = &root
-	pQuadTree := &quadTree
 
 	//insert all stars in the universe into the tree
 	for i := range currentUniverse.stars {
-		if currentUniverse.stars[i].StarOutOfBound(currentUniverse.width) == true {
+		if currentUniverse.stars[i].StarOutOfBound(currentUniverse.width) == false {
 			continue
 		}
-		pQuadTree.root.InsertStar(currentUniverse.stars[i])
+		quadTree.root.InsertStar(currentUniverse.stars[i])
 	}
 
 	return quadTree
@@ -114,61 +188,58 @@ func (star *Star) StarOutOfBound(width float64) bool {
 }
 
 func (proot *Node) InsertStar(star *Star) {
-
 	if proot.children != nil {
 		index := proot.FindInsertIndex(star)
 		proot.children[index].InsertStar(star)
 		proot.UpdateNode(proot.children[index])
+	} else if proot.children == nil && proot.star == nil {
+		proot.star = star
+		return
 	} else {
-		if proot.star == nil {
-			proot.star = star
-			return
-		} else {
-			prootStar := proot.star
-			*proot = proot.MakeDummyNode()
-			proot.InsertStar(prootStar)
-			proot.InsertStar(star)
-		}
+		prootStar := proot.star
+		proot.MakeDummyNode()
+		proot.InsertStar(prootStar)
+		proot.InsertStar(star)
 	}
 }
 
-func (proot *Node) UpdateNode(child *Node){
-	proot.
+func (proot *Node) UpdateNode(child *Node) {
+	proot.star.position.x = (proot.star.position.x*proot.star.mass + child.star.position.x*child.star.mass) / (proot.star.mass + child.star.mass)
+	proot.star.position.y = (proot.star.position.y*proot.star.mass + child.star.position.y*child.star.mass) / (proot.star.mass + child.star.mass)
+	proot.star.mass = proot.star.mass + child.star.mass
 }
 
-func (proot *Node) MakeDummyNode() Node {
-	var dummyNode Node
-	dummyNode.children = make([]*Node, 4)
-	dummyNode.sector.x = proot.sector.x
-	dummyNode.sector.y = proot.sector.y
-	dummyNode.sector.width = proot.sector.width
+func (proot *Node) MakeDummyNode() {
+	proot.children = make([]*Node, 4)
+	var dummyStar Star
+	proot.star = &dummyStar
 	for i := 0; i < 4; i++ {
+		var n Node
 		if i == 1 || i == 3 {
-			dummyNode.children[i].sector.x = proot.sector.x + proot.sector.width/2
+			n.sector.x = proot.sector.x + proot.sector.width/4.0
 		} else {
-			dummyNode.children[i].sector.x = proot.sector.x - proot.sector.width/2
+			n.sector.x = proot.sector.x - proot.sector.width/4.0
 		}
 		if i == 0 || i == 1 {
-			dummyNode.children[i].sector.y = proot.sector.y + proot.sector.width/2
+			n.sector.y = proot.sector.y + proot.sector.width/4.0
 		} else {
-			dummyNode.children[i].sector.y = proot.sector.y - proot.sector.width/2
+			n.sector.y = proot.sector.y - proot.sector.width/4.0
 		}
-		dummyNode.sector.width = proot.sector.width / 2
+		n.sector.width = proot.sector.width / 2.0
+		proot.children[i] = &n
 	}
-
-	return dummyNode
 }
 
 func (proot *Node) FindInsertIndex(star *Star) int {
 
 	if star.position.x < proot.sector.x && star.position.y < proot.sector.y {
-		return 3
-	} else if star.position.x < proot.sector.x && star.position.y > proot.sector.y {
-		return 1
-	} else if star.position.x >= proot.sector.x && star.position.y >= proot.sector.y {
 		return 2
+	} else if star.position.x < proot.sector.x && star.position.y > proot.sector.y {
+		return 0
+	} else if star.position.x >= proot.sector.x && star.position.y >= proot.sector.y {
+		return 1
 	} else {
-		return 4
+		return 3
 	}
 
 }
--- Completed Code/go/src/BarnesHut/main.go
+++ Completed Code/go/src/BarnesHut/main.go
@@ -3,41 +3,162 @@ package main
 import (
 	"fmt"
 	"gifhelper"
+	"math"
+	"os"
 )
 
 func main() {
 
-	// the following sample parameters may be helpful for the "collide" command
-	// all units are in SI (meters, kg, etc.)
-	// but feel free to change the positions of the galaxies.
+	// a scaling factor is needed to inflate size of stars when drawn because galaxies are very sparse
+	universeType := os.Args[1]
+	if universeType == "jupiter" {
+		var jupiter, io, europa, ganymede, callisto Star
 
-	g0 := InitializeGalaxy(500, 4e21, 7e22, 2e22)
-	g1 := InitializeGalaxy(500, 4e21, 3e22, 7e22)
+		jupiter.red, jupiter.green, jupiter.blue = 223, 227, 202
+		io.red, io.green, io.blue = 249, 249, 165
+		europa.red, europa.green, europa.blue = 132, 83, 52
+		ganymede.red, ganymede.green, ganymede.blue = 76, 0, 153
+		callisto.red, callisto.green, callisto.blue = 0, 153, 76
 
-	// you probably want to apply a "push" function at this point to these galaxies to move
-	// them toward each other to collide.
-	// be careful: if you push them too fast, they'll just fly through each other.
-	// too slow and the black holes at the center collide and hilarity ensues.
+		jupiter.mass = 1.898 * math.Pow(10, 27)
+		io.mass = 8.9319 * math.Pow(10, 22)
+		europa.mass = 4.7998 * math.Pow(10, 22)
+		ganymede.mass = 1.4819 * math.Pow(10, 23)
+		callisto.mass = 1.0759 * math.Pow(10, 23)
 
-	width := 1.0e23
-	galaxies := []Galaxy{g0, g1}
+		jupiter.radius = 71000000
+		io.radius = 1821000
+		europa.radius = 1569000
+		ganymede.radius = 2631000
+		callisto.radius = 2410000
 
-	initialUniverse := InitializeUniverse(galaxies, width)
+		jupiter.position.x, jupiter.position.y = 2000000000, 2000000000
+		io.position.x, io.position.y = 2000000000-421600000, 2000000000
+		europa.position.x, europa.position.y = 2000000000, 2000000000+670900000
+		ganymede.position.x, ganymede.position.y = 2000000000+1070400000, 2000000000
+		callisto.position.x, callisto.position.y = 2000000000, 2000000000-1882700000
 
-	// now evolve the universe: feel free to adjust the following parameters.
-	numGens := 500000
-	time := 2e14
-	theta := 0.5
+		jupiter.velocity.x, jupiter.velocity.y = 0, 0
+		io.velocity.x, io.velocity.y = 0, -17320
+		europa.velocity.x, europa.velocity.y = -13740, 0
+		ganymede.velocity.x, ganymede.velocity.y = 0, 10870
+		callisto.velocity.x, callisto.velocity.y = 8200, 0
 
-	timePoints := BarnesHut(initialUniverse, numGens, time, theta)
+		// declaring universe and setting its fields.
+		var jupiterSystem Universe
+		jupiterSystem.width = 4000000000
+		jupiterSystem.stars = []*Star{&jupiter, &io, &europa, &ganymede, &callisto}
 
-	fmt.Println("Simulation run. Now drawing images.")
-	canvasWidth := 1000
-	frequency := 1000
-	scalingFactor := 1e11 // a scaling factor is needed to inflate size of stars when drawn because galaxies are very sparse
-	imageList := AnimateSystem(timePoints, canvasWidth, frequency, scalingFactor)
+		// now evolve the universe: feel free to adjust the following parameters.
+		numGens := 50000
+		time := 50.0
+		theta := 0.5
+		canvasWidth := 1000
+		frequency := 10
+		scalingFactor := 1e1
 
-	fmt.Println("Images drawn. Now generating GIF.")
-	gifhelper.ImagesToGIF(imageList, "galaxy")
-	fmt.Println("GIF drawn.")
+		fmt.Println("checkPoint1")
+
+		timePoints := BarnesHut(&jupiterSystem, numGens, time, theta)
+
+		fmt.Println("Simulation run. Now drawing images.")
+
+		imageList := AnimateSystem(timePoints, canvasWidth, frequency, scalingFactor)
+
+		fmt.Println("Images drawn. Now generating GIF.")
+		gifhelper.ImagesToGIF(imageList, "jupiter")
+		fmt.Println("GIF drawn.")
+
+	} else if universeType == "galaxy" {
+		g0 := InitializeGalaxy(500, 4e21, 7e22, 2e22)
+		width := 1.0e23
+		galaxies := []Galaxy{g0}
+
+		initialUniverse := InitializeUniverse(galaxies, width)
+
+		// now evolve the universe: feel free to adjust the following parameters.
+		numGens := 500
+		time := 2e14
+		theta := 0.5
+
+		timePoints := BarnesHut(initialUniverse, numGens, time, theta)
+
+		fmt.Println("Simulation run. Now drawing images.")
+		canvasWidth := 1000
+		frequency := 1
+		scalingFactor := 1e11 // a scaling factor is needed to inflate size of stars when drawn because galaxies are very sparse
+		imageList := AnimateSystem(timePoints, canvasWidth, frequency, scalingFactor)
+
+		fmt.Println("Images drawn. Now generating GIF.")
+		gifhelper.ImagesToGIF(imageList, "galaxy")
+		fmt.Println("GIF drawn.")
+
+	} else if universeType == "collision" {
+		g0 := InitializeGalaxy(500, 4e21, 7e22, 2e22)
+		g1 := InitializeGalaxy(500, 4e21, 3e22, 7e22)
+
+		// you probably want to apply a "push" function at this point to these galaxies to move
+		// them toward each other to collide.
+		// be careful: if you push them too fast, they'll just fly through each other.
+		// too slow and the black holes at the center collide and hilarity ensues.
+		g0.Push(-5e6, 5e6)
+		g1.Push(5e6, -5e6)
+
+		width := 1.0e23
+		galaxies := []Galaxy{g0, g1}
+
+		initialUniverse := InitializeUniverse(galaxies, width)
+
+		// now evolve the universe: feel free to adjust the following parameters.
+		numGens := 5000
+		time := 2e14
+		theta := 0.5
+
+		timePoints := BarnesHut(initialUniverse, numGens, time, theta)
+
+		fmt.Println("Simulation run. Now drawing images.")
+		canvasWidth := 1000
+		frequency := 10
+		scalingFactor := 1e11 // a scaling factor is needed to inflate size of stars when drawn because galaxies are very sparse
+		imageList := AnimateSystem(timePoints, canvasWidth, frequency, scalingFactor)
+
+		fmt.Println("Images drawn. Now generating GIF.")
+		gifhelper.ImagesToGIF(imageList, "collision")
+		fmt.Println("GIF drawn.")
+
+	}
+
+	// // the following sample parameters may be helpful for the "collide" command
+	// // all units are in SI (meters, kg, etc.)
+	// // but feel free to change the positions of the galaxies.
+
+	// g0 := InitializeGalaxy(500, 4e21, 7e22, 2e22)
+	// g1 := InitializeGalaxy(500, 4e21, 3e22, 7e22)
+
+	// // you probably want to apply a "push" function at this point to these galaxies to move
+	// // them toward each other to collide.
+	// // be careful: if you push them too fast, they'll just fly through each other.
+	// // too slow and the black holes at the center collide and hilarity ensues.
+
+	// width := 1.0e23
+	// galaxies := []Galaxy{g0, g1}
+
+	// initialUniverse := InitializeUniverse(galaxies, width)
+
+	// // now evolve the universe: feel free to adjust the following parameters.
+	// numGens := 500000
+	// time := 2e14
+	// theta := 0.5
+
+	// timePoints := BarnesHut(initialUniverse, numGens, time, theta)
+
+	// fmt.Println("Simulation run. Now drawing images.")
+	// canvasWidth := 1000
+	// frequency := 1000
+	// scalingFactor := 1e11 // a scaling factor is needed to inflate size of stars when drawn because galaxies are very sparse
+	// imageList := AnimateSystem(timePoints, canvasWidth, frequency, scalingFactor)
+
+	// fmt.Println("Images drawn. Now generating GIF.")
+	// gifhelper.ImagesToGIF(imageList, "galaxy")
+	// fmt.Println("GIF drawn.")
 }
